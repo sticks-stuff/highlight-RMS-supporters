c430be40ceb0b9d480fdf7a2f090d7b98b1b54ad
stick <4157860+sticks-stuff@users.noreply.github.com>
2021-03-28 18:13:02 +1300

    lowered the min length and added a filter for the username "event"

--- generate-styles.js
+++ generate-styles.js
@@ -9,14 +9,14 @@ names.forEach(function (item) {
 	link = link.split("?")[0];
 	ghUsername = link.split("github.com/")[1];
 	if(ghUsername != undefined) {
-		if(ghUsername.length > 6) { //basic sanity check
+		if(ghUsername.length > 4 && ghUsername != "event") { //basic sanity check
 			output += `a[href*="${ghUsername.replace(/\/+$/, '')}"], `; //remove the trailing backslash
 		} else {
 			return;
 		}
 	}
 	else {
-		if(link.length > 6) { //basic sanity check
+		if(link.length > 4) { //basic sanity check
 			output += `a[href*="${link}"], `;
 		} else {
 			return;
