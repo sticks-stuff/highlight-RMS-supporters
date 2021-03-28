c1a5d3c3be5e9d68d50ec9b988ba971a6e7a18da
Plikins <44211005+alphonus@users.noreply.github.com>
2021-03-28 23:08:03 +0200

    Changed Background colour

--- generate-styles.js
+++ generate-styles.js
@@ -25,4 +25,4 @@ names.forEach(function (item) {
 });
 
 console.log(output.slice(0, -2));
-console.log("\{ content-before: '\a0'; content-after: '\a0'; background-color: crimson; color: black !important; \}");
+console.log("\{ content-before: '\a0'; content-after: '\a0'; background-color: ForestGreen; color: black !important; \}");
