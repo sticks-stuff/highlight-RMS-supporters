459f3d83b6bd56fa9bd7c7b24a32d5c91808ab95
stick <4157860+sticks-stuff@users.noreply.github.com>
2021-03-31 10:48:18 +1300

    Update generate-styles.js

--- generate-styles.js
+++ generate-styles.js
@@ -10,14 +10,14 @@ names.forEach(function (item) {
 	link = link.split("?")[0];
 	let ghUsername = url.parse(item.link).pathname?.slice(1)
 	if(ghUsername != undefined) {
-		if(ghUsername.length > 4 && ghUsername != "event") { //basic sanity check
+		if(ghUsername.length > 6 && ghUsername != "event") { //basic sanity check
 			output += `a[href*="${ghUsername.replace(/\/+$/, '')}"], `; //remove the trailing backslash
 		} else {
 			return;
 		}
 	}
 	else {
-		if(link.length > 4) { //basic sanity check
+		if(link.length > 6) { //basic sanity check
 			output += `a[href*="${link}"], `;
 		} else {
 			return;
